3f246491ffaddaefdee9b16ab900f5f74121277e
ys2896 <94210536+ys2896@users.noreply.github.com>
2021-11-15 21:42:54 +0900

    Update umpire_LINE.py

--- umpire_LINE.py
+++ umpire_LINE.py
@@ -17,7 +17,7 @@ driver.get(url1)
 #driver.find_element(by=By.XPATH, value="//input[@type='text']")
 #driver.findElement(By.cssSelector("#cheese #cheddar"));
 
-umpire_name = driver.find_Element(By.cssSelector,"#layout > div > div > div.detail_unit > div > dl > dt")
+umpire_name = driver.find_element(By.cssSelector,"#layout > div > div > div.detail_unit > div > dl > dt")
 
 
 message = umpire_name
